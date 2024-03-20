6eb483f70b327df997faf6da7b4007bf5c517d7c
ridwan414-hub <asifmahmoud414@gmail.com>
2024-03-20 16:16:38 +0600

    4-b done

--- part4/tests/reverse.test.js
+++ part4/tests/reverse.test.js
@@ -1,24 +1,26 @@
 /* eslint-disable @stylistic/js/linebreak-style */
 /* eslint-disable @stylistic/js/indent */
-const { test } = require('node:test')
+const { test , describe } = require('node:test')
 const assert = require('node:assert')
 
 const reverse = require('../utils/for-testing').reverse
 
-test('reverse of a', () => {
-    const result = reverse('a')
+describe('Reverse', () => {
+    test('reverse of a', () => {
+        const result = reverse('a')
 
-    assert.strictEqual(result, 'a')
-})
+        assert.strictEqual(result, 'a')
+    })
 
-test('reverse of react', () => {
-    const result = reverse('react')
+    test('reverse of react', () => {
+        const result = reverse('react')
 
-    assert.strictEqual(result, 'tkaer')
-})
+        assert.strictEqual(result, 'tcaer')
+    })
 
-test('reverse of saippuakauppias', () => {
-    const result = reverse('saippuakauppias')
+    test('reverse of saippuakauppias', () => {
+        const result = reverse('saippuakauppias')
 
-    assert.strictEqual(result, 'saippuakauppias')
+        assert.strictEqual(result, 'saippuakauppias')
+    })
 })
