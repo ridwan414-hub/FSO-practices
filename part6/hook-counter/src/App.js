68f9332100eb3392d1246745a8db3e92245aadcf
ridwan414-hub <asifmahmoud414@gmail.com>
2024-04-02 00:27:08 +0600

    part 6 finished

--- part6/hook-counter/src/App.js
+++ part6/hook-counter/src/App.js
@@ -1,28 +1,15 @@
-import { useReducer } from 'react'
+import Button from "./components/Button"
+import Display from "./components/Display"
 
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "INC":
-        return state + 1
-    case "DEC":
-        return state - 1
-    case "ZERO":
-        return 0
-    default:
-        return state
-  }
-}
 
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0)
-
   return (
     <div>
-      <div>{counter}</div>
+      <Display />
       <div>
-        <button onClick={() => counterDispatch({ type: "INC"})}>+</button>
-        <button onClick={() => counterDispatch({ type: "DEC"})}>-</button>
-        <button onClick={() => counterDispatch({ type: "ZERO"})}>0</button>
+        <Button type='INC' label='+' />
+        <Button type='DEC' label='-' />
+        <Button type='ZERO' label='0' />
       </div>
     </div>
   )
