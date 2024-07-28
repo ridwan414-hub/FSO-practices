2471e8313196637bc48242ef8ba3061fdc72c8fd
ridwan414-hub <asifmahmoud414@gmail.com>
2024-07-28 16:05:33 +0600

    ends with 9-b

---
+++ part9/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import { calculator, Operation } from './calculator';
+const app = express();
+app.use(express.json());
+
+app.get('/ping', (_req, res) => {
+  res.send('pong');
+});
+
+app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  // validate the data here
+
+  const result = calculator(Number(value1), Number(value2), op as Operation);
+
+  return res.send({ result });
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
