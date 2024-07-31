29f96b01c9d2a79e8f4b8a16bb8e57b38672ccf8
ridwan414-hub <asifmahmoud414@gmail.com>
2024-07-31 11:37:10 +0600

    utils ends me

---
+++ part9/flight-diary/src/types.ts
@@ -0,0 +1,13 @@
+export interface DiaryEntry {
+  id: number;
+  date: string;
+  weather: Weather;
+  visibility: Visibility;
+  comment: string;
+}
+export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>;
+export type NewDiaryEntry = Omit<DiaryEntry, 'id'>;
+
+export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
+
+export type Visibility = 'great' | 'good' | 'ok' | 'poor';
