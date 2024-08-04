5f8e6df0f6623611c7cab8b0385412eff0e92d5c
ridwan414-hub <asifmahmoud414@gmail.com>
2024-08-04 08:29:33 +0600

    9-c end with ts utils and types

--- part9/flight-diary/src/types.ts
+++ part9/flight-diary/src/types.ts
@@ -8,6 +8,17 @@ export interface DiaryEntry {
 export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>;
 export type NewDiaryEntry = Omit<DiaryEntry, 'id'>;
 
-export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
+export enum Weather {
+  Sunny = 'sunny',
+  Rainy = 'rainy',
+  Cloudy = 'cloudy',
+  Stormy = 'stormy',
+  Windy = 'windy',
+}
 
-export type Visibility = 'great' | 'good' | 'ok' | 'poor';
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor',
+}
